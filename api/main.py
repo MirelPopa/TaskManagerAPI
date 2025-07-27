6c35165b2bf53c501dfbecd08b21eea6ff00d0fc
Mirel Popa <mirel_0795@yahoo.com>
2025-07-27 18:46:31 +0300

    *added grafana and prometheus

--- api/main.py
+++ api/main.py
@@ -1,6 +1,8 @@
+import time
 from typing import List
+from prometheus_client import start_http_server, Counter, Summary, generate_latest, CONTENT_TYPE_LATEST
 
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, Response
 from sqlalchemy import insert, select
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Session
@@ -10,7 +12,24 @@ from api.models import Task
 from api.schema import TaskBase, TaskCreate, TaskRead, TaskStatus
 from worker.main import generate_report
 
+
+REQUEST_COUNT = Counter("request_count", "Total number of requests")
+REQUEST_LATENCY = Summary("request_latency_seconds", "Latency of requests in seconds")
 router = FastAPI()
+
+
+@router.middleware("http")
+async def prometheus_middleware(request, call_next):
+    REQUEST_COUNT.inc()
+    start_time = time.time()
+    response = await call_next(request)
+    latency = time.time() - start_time
+    REQUEST_LATENCY.observe(latency)
+    return response
+
+@router.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
 
 
 @router.get(path="/tasks", response_model=List[TaskRead])
