e907a22c48400cface819a3265e3527fe44684c0
Mirel Popa <mirel_0795@yahoo.com>
2025-07-27 14:31:29 +0300

    *corrected the test database url in the conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import sessionmaker
 from api.db import Base
 from api.models import Task
 
-DB_URL = os.getenv("DATABASE_URL")
+DB_URL = os.getenv("TEST_DATABASE_URL")
 
 engine = create_engine(DB_URL)
 TestingSessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
